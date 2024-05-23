38eb814719ceeb43a7956f348273877a0a9cb283
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-23 19:19:36 +0100

    Button form, JS to extract button with id

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const embeddedDiv = document.getElementById('embedded_div')
+const button = document.getElementById('click_me_button')
 
-for (let i = 0; i < 1000; i++) {
-  const newParagraph = document.createElement('p')
-  newParagraph.innerText = `<p>This is paragraph number ${i}</p>`
-  embeddedDiv.append(newParagraph)
-}
+console.log(button)
