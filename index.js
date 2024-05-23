105dc6564a848a0a7b0e9d14cb3aa7688deda73f
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-23 19:06:07 +0100

    Some basic DOM manipulation in JS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('HELLO')
+document.write('Some text added by Javascript')
+
+const embeddedDiv = document.getElementById('embedded_div')
+
+// console.log(embeddedDiv)
+
+// embeddedDiv.innerText = 'I am replacing what was there before'
+// embeddedDiv.append('Hello world')
+console.log(embeddedDiv.textContent)
