69ce745c6373a29b2b27217a6a0607a014ad635e
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-23 19:50:24 +0100

    Strategy for approaching reset button functionality

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ let count = 0
 const button = document.getElementById('click_me_button')
 const counterSpan = document.getElementById('counter')
 
-const clickHandler = () => {
+/*
+ * - Add a new reset button
+ * - Add a click handler to the new button
+ * - In the click handler, update the count
+ * - DRY up the code (DRY = Don't repeat yourself)
+ */
+
+button.addEventListener('click', () => {
   count++
   counterSpan.innerText = count
-}
-
-// button.addEventListener('click', () => {
-//   count++
-//   counterSpan.innerText = count
-// })
+})
