7559a3653af0c31c2b3e8af81a332fbbcd5f4e2f
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-23 19:23:40 +0100

    Populate the span counter placeholder with the count

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let count = 0
 
 const button = document.getElementById('click_me_button')
+const counterSpan = document.getElementById('counter')
+
+console.log(counterSpan)
 
 button.addEventListener('click', () => {
   count ++
-  console.log(count)
+  counterSpan.innerText = count
 })
