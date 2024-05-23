d1b350f7f132aff71170562379dc891580beb294
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-23 19:11:03 +0100

    DOM manipulation with a loop

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-document.write('Some text added by Javascript')
-
 const embeddedDiv = document.getElementById('embedded_div')
 
-// console.log(embeddedDiv)
-
-// embeddedDiv.innerText = 'I am replacing what was there before'
-// embeddedDiv.append('Hello world')
-console.log(embeddedDiv.textContent)
+for (let i = 0; i < 1000; i++) {
+  const newParagraph = document.createElement('p')
+  newParagraph.innerText = `<p>This is paragraph number ${i}</p>`
+  embeddedDiv.append(newParagraph)
+}
